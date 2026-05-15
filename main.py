43ad101cac0199c640438844777d3a5d0884d524
savi4m83-crypto <savi4m83@gmail.com>
2026-05-15 11:57:01 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+name=str(input("Enter your name "))
+age=int(input("Enter your age "))
+weight=float(input("Enter your weight "))
+is_student=bool(input("Are you a student "))
+is_student=True
+print("yes")
+if is_student:=False:
+    print("no")
+print(type(name))
+print(type(age))
+print(type(weight))
+print(type(is_student))
+age=int
+age=str(age)
+print(type(age))
+weight=float
+weight=str(weight)
+print(type(weight))
